c6239e7d4ee54cd6e511b3c4558fe01414533442
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-01-31 10:18:37 +0500

    Добавьте файлы проекта.

---
+++ VoiceTexterBot/Bot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace VoiceTexterBot
+{
+    internal class Bot
+    {
+        private ITelegramBotClient _telegramClient;
+
+        public Bot(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+    }
+}
