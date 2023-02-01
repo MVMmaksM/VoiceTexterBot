5f2226a61a3a4406ccff15d7f83c53789c7556b4
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-02-01 10:37:09 +0500

    Create class Bot

--- VoiceTexterBot/Bot.cs
+++ VoiceTexterBot/Bot.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualBasic;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bots.Extensions.Polling;
 
 namespace VoiceTexterBot
 {
@@ -15,5 +15,38 @@ namespace VoiceTexterBot
         {
             _telegramClient = telegramClient;
         }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            //нажатие на кнопки
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id,"Вы нажали на кнопку", cancellationToken:cancellationToken);
+                return;
+            }
+
+            if (update.Type == UpdateType.Message)
+            {
+                await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Отправилви сообщение", cancellationToken: cancellationToken);
+                return;
+            }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken) 
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(errorMessage);
+
+            Console.WriteLine("Ожидание 10 секунд перед повторным подключением");
+            Thread.Sleep(10000);
+
+            return Task.CompletedTask;
+        }
+
     }
 }
