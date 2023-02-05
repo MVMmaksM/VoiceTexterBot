e2cf231c6d36050e6321490609674d089f0c10a7
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-05 17:06:51 +0500

    Update Process() in AudioFileHandler

--- VoiceTexterBot/Services/AudioFileHandler .cs
+++ VoiceTexterBot/Services/AudioFileHandler .cs
@@ -46,7 +46,10 @@ namespace VoiceTexterBot.Services
             AudioConverter.TryConvert(inputAudioPath, outputAudioPath);
             Console.WriteLine("Файл конвертирован");
 
-            return "Конвертация успешно завершена";
+            Console.WriteLine("Начинаем распознавание...");
+            var speechText = SpeechDetector.DetectSpeech(outputAudioPath, _appSettings.InputAudioBitrate, languageCode);
+            Console.WriteLine("Файл распознан.");
+            return speechText;
         }
     }
 }
