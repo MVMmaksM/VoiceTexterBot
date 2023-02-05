322f77e75f21a9a241e43587280c61e53705c185
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-05 13:27:21 +0500

    Create TextMessageController

---
+++ VoiceTexterBot/Controllers/TextMessageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace VoiceTexterBot.Controllers
+{
+    internal class TextMessageController
+    {
+        private readonly ITelegramBotClient _telegramBotClient;
+
+        public TextMessageController(ITelegramBotClient telegramBotClient)
+        {
+            _telegramBotClient = telegramBotClient;
+        }
+
+    }
+}
