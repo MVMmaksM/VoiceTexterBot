d4e715d3294e0a9298f26bbd4e25294bd9ff1d32
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-05 13:05:28 +0500

    create utilitybot

---
+++ UtilityBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+using Telegram.Bot;
+
+namespace UtilityBot
+{
+    internal class Program 
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder().ConfigureServices((hostcontext, services) => ConfigureServices(services)).UseConsoleLifetime().Build();
+
+            Console.WriteLine("Сервис запущен");
+
+            await host.RunAsync();
+
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6156594854:AAGGUk0mvAyxoMOiq804ir0IRJ5CX9POk9U"));
+
+            services.AddHostedService<Bot>();
+        }
+    }
+}
+
