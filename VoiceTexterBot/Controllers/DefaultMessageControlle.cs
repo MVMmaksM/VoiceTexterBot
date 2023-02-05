0d8196fb86793316f156fdb03c7d795a82dc105f
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-05 13:34:58 +0500

    Create Handle() in DefaultMessageControlle

--- VoiceTexterBot/Controllers/DefaultMessageControlle.cs
+++ VoiceTexterBot/Controllers/DefaultMessageControlle.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace VoiceTexterBot.Controllers
 {
@@ -14,6 +15,12 @@ namespace VoiceTexterBot.Controllers
         public DefaultMessageControlle(ITelegramBotClient telegramBotClient)
         {
             _telegramBotClient = telegramBotClient;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            await _telegramBotClient.SendTextMessageAsync(message.Chat.Id, $"Получено сообщение не поддерживаемого формата", cancellationToken: ct);
         }
     }
 }
