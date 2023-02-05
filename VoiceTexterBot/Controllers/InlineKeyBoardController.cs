8d0c090c66277fe8d0950985c84377ab9a7e104f
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-05 13:37:27 +0500

    Create Handle() in InlineKeyBoardController

--- VoiceTexterBot/Controllers/InlineKeyBoardController.cs
+++ VoiceTexterBot/Controllers/InlineKeyBoardController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace VoiceTexterBot.Controllers
 {
@@ -14,6 +15,13 @@ namespace VoiceTexterBot.Controllers
         public InlineKeyBoardController(ITelegramBotClient telegramBotClient)
         {
             _telegramBotClient = telegramBotClient;
+        }
+
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
+
+            await _telegramBotClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку", cancellationToken: ct);
         }
     }
 }
