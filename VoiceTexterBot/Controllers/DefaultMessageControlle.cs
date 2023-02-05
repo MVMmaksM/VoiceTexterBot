d5c49ed6b12e81eae754b7000939f0c4c3ade510
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-05 13:30:28 +0500

    Create DefaultMessageControlle

---
+++ VoiceTexterBot/Controllers/DefaultMessageControlle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace VoiceTexterBot.Controllers
+{
+    internal class DefaultMessageControlle
+    {
+        private readonly ITelegramBotClient _telegramBotClient;
+
+        public DefaultMessageControlle(ITelegramBotClient telegramBotClient)
+        {
+            _telegramBotClient = telegramBotClient;
+        }
+    }
+}
