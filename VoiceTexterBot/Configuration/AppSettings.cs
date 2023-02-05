3b1d49e423e1c7402873080f7b490f4ca53225a7
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-05 16:41:18 +0500

    Update class AppSettings

--- VoiceTexterBot/Configuration/AppSettings.cs
+++ VoiceTexterBot/Configuration/AppSettings.cs
@@ -9,5 +9,8 @@ namespace VoiceTexterBot.Configuration
     internal class AppSettings
     {
         public string BotToken { get; set; }
+        public string DownloadsFolder { get; set; }
+        public string AudioFileName { get; set; }
+        public string InputAudioFormat { get; set; }
     }
 }
