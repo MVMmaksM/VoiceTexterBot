fe4637da86f4d0e2093750d81c9c70621c77e36d
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-05 13:31:39 +0500

    Create InlineKeyBoardController

---
+++ VoiceTexterBot/Controllers/InlineKeyBoardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace VoiceTexterBot.Controllers
+{
+    internal class InlineKeyBoardController
+    {
+        private readonly ITelegramBotClient _telegramBotClient;
+
+        public InlineKeyBoardController(ITelegramBotClient telegramBotClient)
+        {
+            _telegramBotClient = telegramBotClient;
+        }
+    }
+}
